6496fb738bb1b75c715c856c509cd03234a2896b
thinh-dao <ducthinh19102003@gmail.com>
2025-05-15 13:34:23 +0700

    update settings

--- experiments/anomaly_detection_singleshot.sh
+++ experiments/anomaly_detection_singleshot.sh
@@ -1,4 +1,3 @@
-# CIFAR
 python main.py -m \
     aggregator=flame,deepsight,rflbat,ad_multi_krum,indicator,fldetector \
     atk_config=singleshot \
@@ -10,5 +9,29 @@ python main.py -m \
     save_logging=csv \
     num_gpus=0.5 \
     num_cpus=1 \
-    cuda_visible_devices=\"0,1,2,3,4\" \
-    dir_tag=anomaly_detection_cifar10
+    cuda_visible_devices=\"0,1,2,4,5\" \
+    dir_tag=anomaly_detection_cifar10 && \
+python main.py -m \
+    aggregator=flame,deepsight,rflbat,ad_multi_krum,indicator,fldetector \
+    num_clients=3383 \
+    num_clients_per_round=30 \
+    dataset=emnist_byclass \
+    test_batch_size=512 \
+    num_workers=8 \
+    model=mnistnet \
+    atk_config=emnist_singleshot \
+    atk_config.data_poison_method=pattern \
+    atk_config.poison_start_round=1301 \
+    atk_config.poison_end_round=1301 \
+    checkpoint=1300 \
+    num_rounds=100 \
+    num_rounds=1 \
+    save_logging=csv \
+    num_gpus=0.5 \
+    num_cpus=1 \
+    cuda_visible_devices=\"0,1,2,4,5\" \
+    dir_tag=robust_aggregation_singleshot
+
+
+
+
