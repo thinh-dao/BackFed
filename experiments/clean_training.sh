eeeec94a4c51146749be36cef37a603a3bba71f0
Thinh Dao <ducthinh19102003@gmail.com>
2025-05-22 05:51:04 +0000

    update clean_training for sentiment140

--- experiments/clean_training.sh
+++ experiments/clean_training.sh
@@ -6,3 +6,6 @@ python main.py dataset=cifar10 num_rounds=2000 num_clients=100 model=resnet18 no
 
 # *TINYIMAGENET
 python main.py dataset=tinyimagenet client_config.local_epochs=5 num_rounds=3000 num_clients=200 test_batch_size=1024 test_every=5 num_clients_per_round=10 model=resnet18 no_attack=True cuda_visible_devices=\"1,5,7,2,4\" save_checkpoint=True "save_model_rounds=[500,1000,1500,2000,2500,3000]" checkpoint=Null pretrain_model_path=IMAGENET1K_V1
+
+# *Sentiment140
+python main.py -cn reddit num_rounds=100 no_attack=True cuda_visible_devices=\"0,1,2,3,4\"
